426b91215c093e3f7fe611d276b4163fd6de6f43
Antoine Choppin <antoine@japonophile.com>
2020-05-01 23:37:26 +0900

    Simplify robot model.  Looks OK.

--- 2020/urdf/main.py
+++ 2020/urdf/main.py
@@ -5,14 +5,13 @@ import math
 import sys
 
 
-# DARWIN_URDF = 'darwin_orig.urdf'
 DARWIN_URDF = 'darwin.urdf'
 robot = URDF.load(DARWIN_URDF)
 joint_names = list(filter(lambda j: j.joint_type != 'fixed', robot.joints))
 default_angle = math.pi * 150 / 180
 cfg = dict(zip(joint_names, [default_angle] * len(joint_names)))
+cfg['l_biceps_joint'] = math.pi * 210 / 180
 robot.show(cfg=cfg)
-sys.exit(0)
 
 
 def print_link(l, indent=''):
@@ -32,22 +31,9 @@ print_link(robot.base_link)
 # chain_elements = get_chain_from_joints(DARWIN_URDF, motors)
 #  -> this does not work due to StopIteration!  How does this code even exist???
 
-chain_elements = [
-'body_skeleton_link',
-'body_torso_joint', 'body_torso_link',
-'neck_axis_joint', 'neck_axis_link',
-'neck_joint', 'neck_link',
-'body_head_joint', 'body_head_link',
-'body_back_joint', 'body_back_link',
-'body_chest_joint', 'body_chest_link',
-'l_shoulder_anchor_joint', 'l_shoulder_anchor_link',
-'l_shoulder_servo_joint', 'l_shoulder_servo_link',
-'l_shoulder_joint', 'l_shoulder_link',
-'l_biceps_anchor_joint', 'l_biceps_anchor_link',
-'l_biceps_axis_joint', 'l_biceps_axis_link',
-'l_biceps_joint', 'l_biceps_link',
-'l_biceps_spo_joint',
-]
+chain_elements = ['body_link', 'l_shoulder_joint', 'l_shoulder_link',
+                  'l_biceps_joint', 'l_biceps_link', 'l_elbow_joint',
+                  'l_hand_link']
 
 chain = Chain.from_urdf_file(DARWIN_URDF, base_elements=chain_elements)
 print(chain)
