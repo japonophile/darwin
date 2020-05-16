f9a5a6108e9667a7853e138b982aa7a640c33888
Antoine Choppin <antoine@japonophile.com>
2020-05-16 23:08:16 +0900

    Start working on gait generation

---
+++ README.md
@@ -0,0 +1,19 @@
+# Darwin Mini
+
+
+
+
+### How to build the V-REP scene (for use with CoppeliaSim simulator)
+
+- Follow the explanations here: https://www.coppeliarobotics.com/helpFiles/en/buildingAModelTutorial.htm
+- The Collada import plugin is broken so can only be used once per session (to use it again, need to restart CoppeliaSim)
+- Import .dae composed of multiple meshes, but do not check the checkbox to group them, as it would loose their individual material (color) information.  Rather, group them after import through the right click menu.
+- Important!  Connect joint to respondable body, and respondable body to joint.  Visual body is connected to the respondable body, but NOT in the dynamic chain.
+
+
+### Issues
+
+- when starting the notebook to connect to the V-REP simulator, make sure no other client is connected to V-REP or the connection will fail
+- need to use `ikpy==2.3.3`, as it pypot is not compatible with the latest version (3.x)
+- the wrapper around system time (`from . import pypot_time as time` in `stoppablethread.py`) caused threads not to start properly -> had to get rid of this functionality in pypot
+
--- software/darwin_mini/main.py
+++ software/darwin_mini/main.py
@@ -33,7 +33,7 @@ cfg['r_thigh_joint'] = ang(-60)
 cfg['r_knee_joint'] = ang(30)
 cfg['r_ankle_joint'] = ang(30)
 cfg['r_foot_joint'] = ang(30)
-robot.show(cfg=cfg)
+# robot.show(cfg=cfg)
 # robot.animate()
 
 
@@ -54,11 +54,37 @@ print_link(robot.base_link)
 # chain_elements = get_chain_from_joints(DARWIN_URDF, motors)
 #  -> this does not work due to StopIteration!  How does this code even exist???
 
-chain_elements = ['body_link', 'l_shoulder_joint', 'l_shoulder_link',
-                  'l_biceps_joint', 'l_biceps_link', 'l_elbow_joint',
-                  'l_hand_link']
 
-chain = Chain.from_urdf_file(DARWIN_URDF, base_elements=chain_elements)
-print(chain)
+def get_chain(name_templates, side):
+    assert side in ['l', 'r']
+    element_names = ['body_link'] + list(map(lambda s: s.format(side), name_templates))
+    return Chain.from_urdf_file(DARWIN_URDF, base_elements=element_names)
+
+
+arm_element_names = ['{}_shoulder_joint', '{}_shoulder_link',
+                     '{}_biceps_joint', '{}_biceps_link',
+                     '{}_elbow_joint', '{}_hand_link']
+leg_element_names = ['{}_hip_joint', '{}_hip_link',
+                     '{}_thigh_joint', '{}_thigh_link',
+                     '{}_knee_joint', '{}_lowerleg_link',
+                     '{}_ankle_joint', '{}_ankle_link',
+                     '{}_foot_joint', '{}_foot_link']
+
+left_arm = get_chain(arm_element_names, 'l')
+right_arm = get_chain(arm_element_names, 'r')
+left_leg = get_chain(leg_element_names, 'l')
+right_leg = get_chain(leg_element_names, 'r')
+
+# chain_elements = ['body_link', 'l_shoulder_joint', 'l_shoulder_link',
+#                   'l_biceps_joint', 'l_biceps_link', 'l_elbow_joint',
+#                   'l_hand_link']
+
+# chain = Chain.from_urdf_file(DARWIN_URDF, base_elements=chain_elements)
+# print(chain)
+
+print('left_arm', left_arm)
+print('right_arm', right_arm)
+print('left_leg', left_leg)
+print('right_leg', right_leg)
 
 
